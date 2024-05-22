8c8fc39a4de33607663b13b60f5a92fdf1230408
Jan-Rychlik-sh <119613322+Jan-Rychlik-sh@users.noreply.github.com>
2024-05-22 17:59:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dist/style.css">
+
+</head>
+
+<body>
+
+    <header>
+        <a href="index.php" title="Powrót do strony głównej">
+            <h1>Wybierz</h1>
+        </a>
+    </header>
+    <main id="container">
+        <div class="concact_option">
+            <h2><a href="matematyka/index.php">Matematyka</a></h2>
+        </div>
+        <div class="concact_option">
+            <h2><a href="zawodowe/index.php">Zawodowe</a></h2>
+        </div>
+        <div class="concact_option">
+            <h2><a href="kontakt.php">Kontakt</a></h2>
+        </div>
+    </main>
+    <footer>
+        Wykonanie strony - Joachim Rychliński
+    </footer>
+</body>
+
+</html>
---
+++ kontakt.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dist/style.css">
+
+</head>
+
+<body>
+
+    <header>
+        <a href="index.php" title="Powrót do strony głównej">
+            <h1>Kontakt z Maksymilianem Rychlińskim</h1>
+        </a>
+    </header>
+    <main id="container">
+        <div class="concact_option">Telefon: <a href="tel:+48536301320">+48 536 301 320</a></div>
+        <div class="concact_option">E-mail: <a href="mailto:maxfrodo@wp.pl">maxfrodo@wp.pl</a></div>
+        <div class="concact_option">Media: <a href="https://www.e-korepetycje.net/maxmat" target="_blank">e-korepetycje.net</a></div>
+        <div class="concact_option"><a href="https://www.facebook.com/profile.php?id=61550970885129" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Facebook</a></div>
+        <div class="concact_option">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.instagram.com/rychlinskimaks?igsh=MXNzemdoa3hrbjhxbQ==" target="_blank">Instagram</a></div>
+        <figure><img src="wizytowka.png" width="500" alt="wizytówka">
+            <figcaption>Wizytowka - Maks Rychliński</figcaption>
+        </figure>
+    </main>
+    <footer>
+        Wykonanie strony - Joachim Rychliński
+    </footer>
+</body>
+
+</html>
