8901b0d938a369497798b0540dc9a05a3853375f
Jan-Rychlik-sh <119613322+Jan-Rychlik-sh@users.noreply.github.com>
2024-05-22 18:00:57 +0200

    Add files via upload

---
+++ zawodowe/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>😎 Maxmat - Arkusze</title>
+    <link rel="stylesheet" href="../dist/style.css">
+</head>
+
+<body>
+    <header>
+        <a href="../index.php">
+            <h1>Opracowania - Wykonał Maksymilian Rychliński</h1>
+        </a>
+    </header>
+    <main id="container">
+        <details>
+            <summary>Technik Elektryk</summary>
+            <div>
+                <h2><a href="elektryk/E07.php">Egzaminy E.07 (uporządkowane latami)</a></h2>
+                <h2><a href="elektryk/E08.php">Egzaminy E.08 (uporządkowane latami)</a></h2>
+                <h2><a href="elektryk/E24.php">Egzaminy E.24 (uporządkowane latami)</a></h2>
+                <h2><a href="elektryk/EE05.php">Egzaminy EE.05 (uporządkowane latami)</a></h2>
+                <h2><a href="elektryk/EE26.php">Egzaminy EE.26 (uporządkowane latami)</a></h2>
+                <h2><a href="elektryk/ELE02.php">Egzaminy ELE.02 (uporządkowane latami)</a></h2>
+                <h2><a href="elektryk/ELE05.php">Egzaminy ELE.05 (uporządkowane latami)</a></h2>
+            </div>
+        </details>
+        <details>
+            <summary>Technik Automatyk</summary>
+            <div>
+                <h2><a href="automatyk/M16.php">Egzaminy M.16 (uporządkowane latami)</a></h2>
+                <h2><a href="automatyk/MG16.php">Egzaminy MG.16 (uporządkowane latami)</a></h2>
+                <h2><a href="automatyk/EE17.php">Egzaminy EE.17 (uporządkowane latami)</a></h2>
+                <h2><a href="automatyk/EE18.php">Egzaminy EE.18 (uporządkowane latami)</a></h2>
+                <h2><a href="automatyk/ELM01.php">Egzaminy ELM.01 (uporządkowane latami)</a></h2>
+                <h2><a href="automatyk/ELM04.php">Egzaminy ELM.04 (uporządkowane latami)</a></h2>
+            </div>
+        </details>
+        <details>
+            <summary>Technik Informatyk - Programista</summary>
+            <div>
+                <h2><a href="IT/INF02.php">Egzaminy INF.02(uporządkowane latami)</a></h2>
+                <h2><a href="IT/INF03.php">Egzaminy INF.03(uporządkowane latami)</a></h2>
+                <h2><a href="IT/INF04.php">Egzaminy INF.04(uporządkowane latami)</a></h2>
+            </div>
+        </details>
+        <h2>
+            <a href="kontakt.php">Kontakt</a>
+        </h2>
+    </main>
+    <footer>
+        Wykonanie strony - Joachim Rychliński
+    </footer>
+</body>
+
+</html>
